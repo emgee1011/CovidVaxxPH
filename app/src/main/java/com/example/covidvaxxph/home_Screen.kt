d233ed401bbcd66a1c5db1121da9f84c81b97c29
emgee1011 <b2023mglarisca@pshs.edu.ph>
2022-03-21 15:44:06 +0800

    hm

---
+++ app/src/main/java/com/example/covidvaxxph/home_Screen.kt
@@ -0,0 +1,43 @@
+package com.example.covidvaxxph
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class home_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+
+        //ImageButton imageButton1 = (ImageButton) findViewById(R.Id.imageButton1)
+        val imageButton1 = findViewById<ImageButton>(R.id.imageButton1)
+        imageButton1.setOnClickListener{
+            val intent = Intent(this, user_guide::class.java)
+            startActivity(intent)
+        }
+
+        val imageButton2 = findViewById<ImageButton>(R.id.imageButton2)
+        imageButton2.setOnClickListener{
+            val intent = Intent(this, vaxx_info::class.java)
+            startActivity(intent)
+        }
+
+        val imageButton3 = findViewById<ImageButton>(R.id.imageButton3)
+        imageButton3.setOnClickListener{
+            val intent = Intent(this, vaxx_ph::class.java)
+            startActivity(intent)
+        }
+
+        val imageButton4 = findViewById<ImageButton>(R.id.imageButton4)
+        imageButton4.setOnClickListener{
+            val intent = Intent(this,interactive_page::class.java)
+            startActivity(intent)
+        }
+
+        
+
+    }
+
+
+}
